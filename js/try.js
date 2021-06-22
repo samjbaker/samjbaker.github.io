1160f9179f6a5fcc7f9471d37260e1c8fdede8d4
Sam <samjbaker1234@gmail.com>
2021-06-22 16:10:42 +0100

    Trying to import model 2

--- js/try.js
+++ js/try.js
@@ -28,28 +28,9 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 const loader = new GLTFLoader();
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
 loader.load( './models/github_logo.glb', function ( gltf ) {
 
-logo = gltf.scene
-
-	scene.add( logo );
-
-   const animate = function () {
-      requestAnimationFrame( animate );
-
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-
-      renderer.render( scene, camera );
-   };
-
-   animate();
+	scene.add( gltf.scene );
 
 }, undefined, function ( error ) {
 
