95cee95bfda0d75e52cdffc5e0299e8b0c382e73
Sam <samjbaker1234@gmail.com>
2021-11-20 10:16:06 +0000

    changed CSS ref

--- dist/main.js
+++ dist/main.js
@@ -1,6 +1,4 @@
 /*npm run dev to deploy to localhost 3000 */
-import './style.css'
-
 import * as THREE from 'https://unpkg.com/three@0.127.0/build/three.module.js';
 
 import {OrbitControls} from 'https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js';
